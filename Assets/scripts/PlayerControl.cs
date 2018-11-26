002e2c32c3fd4512d27b8d387c09831d9770e0de
mnindrazaka <mnindrazaka@gmail.com>
2018-11-26 18:31:46 +0700

    initial commit

---
+++ Assets/scripts/PlayerControl.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+  Rigidbody2D myRigidbody;
+  float speed = 10;
+
+  bool isGround = false;
+
+  // Use this for initialization
+  void Start()
+  {
+    myRigidbody = GetComponent<Rigidbody2D>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    float horizontal = Input.GetAxis("Horizontal");
+    myRigidbody.velocity = new Vector2(horizontal * speed, myRigidbody.velocity.y);
+
+
+    if (Input.GetButtonDown("Jump") && isGround)
+    {
+      myRigidbody.AddForce(new Vector2(0, 15), ForceMode2D.Impulse);
+    }
+  }
+
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.CompareTag("Ground"))
+    {
+      isGround = true;
+      Debug.Log(isGround);
+    }
+    else if (other.CompareTag("Meat"))
+    {
+      Destroy(other.gameObject);
+    }
+  }
+
+  void OnTriggerExit2D(Collider2D other)
+  {
+    if (other.CompareTag("Ground"))
+    {
+      isGround = false;
+      Debug.Log(isGround);
+    }
+    else if (other.CompareTag("Meat"))
+    {
+      Destroy(other.gameObject);
+    }
+  }
+}
