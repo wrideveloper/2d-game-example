a5adcb62c10ca2171be265fff6ca0ac108143a39
mnindrazaka <mnindrazaka@gmail.com>
2018-11-28 13:38:55 +0700

    add score

--- Assets/scripts/PlayerControl.cs
+++ Assets/scripts/PlayerControl.cs
@@ -1,14 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerControl : MonoBehaviour
 {
+  public Text scoreText;
 
   Rigidbody2D myRigidbody;
   float speed = 10;
 
   bool isGround = false;
+
+  int score = 0;
+
+  Vector3[] meatPosition = {
+    new Vector3(15.28F, 8.4F, 0),
+    new Vector3(-11.5F, 8.7F, 0),
+    new Vector3(6.2F, 4.4F, 0),
+    new Vector3(-6, 0.7F, 0),
+    new Vector3(-13.9F, -3.1F, 0)
+  };
 
   // Use this for initialization
   void Start()
@@ -22,11 +34,15 @@ public class PlayerControl : MonoBehaviour
     float horizontal = Input.GetAxis("Horizontal");
     myRigidbody.velocity = new Vector2(horizontal * speed, myRigidbody.velocity.y);
 
-
     if (Input.GetButtonDown("Jump") && isGround)
     {
-      myRigidbody.AddForce(new Vector2(0, 15), ForceMode2D.Impulse);
+      Jump();
     }
+  }
+
+  void Jump()
+  {
+    myRigidbody.AddForce(new Vector2(0, 15), ForceMode2D.Impulse);
   }
 
   void OnTriggerEnter2D(Collider2D other)
@@ -34,11 +50,14 @@ public class PlayerControl : MonoBehaviour
     if (other.CompareTag("Ground"))
     {
       isGround = true;
-      Debug.Log(isGround);
     }
     else if (other.CompareTag("Meat"))
     {
-      Destroy(other.gameObject);
+      int index = Random.Range(0, 4);
+      other.transform.position = meatPosition[index];
+      score++;
+
+      scoreText.text = score.ToString();
     }
   }
 
@@ -47,11 +66,6 @@ public class PlayerControl : MonoBehaviour
     if (other.CompareTag("Ground"))
     {
       isGround = false;
-      Debug.Log(isGround);
-    }
-    else if (other.CompareTag("Meat"))
-    {
-      Destroy(other.gameObject);
     }
   }
 }
